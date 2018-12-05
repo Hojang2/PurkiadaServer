3c11f9c7a21c30445aa1e680896c4e69fd4a7355
Jan Buchmaier <janbuchmaier@seznam.cz>
2018-12-05 13:55:54 +0100

    First commit for version 2019. Added main.py file

---
+++ Purkiada2019/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import click
+
+@click.command()
+@click.option('--server', type=click.Path(exists=True), help='Path to server')
+@click.option('--host', default='0.0.0.0' help='Adress on where will be server running')
+@click.option('--port', default=9600, help='Port on where the app will run')
+
+def main(server, host, port) -> None:
+    """
+
+    The main file for running Purkyada server
+
+    """
+
+    pass
+
+if __name__ == '__main__':
+    main()
+
