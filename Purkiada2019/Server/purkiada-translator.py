14fdecfb6559b2a679d84df6a148732c6a9d85ca
Jan Buchmaier <janbuchmaier@seznam.cz>
2018-12-22 21:20:42 +0100

    Deleted unused files, rewritten purkiada-server, created Server/json directory for json files

--- Purkiada2019/Server/purkiada-translator.py
+++
@@ -1,9 +0,0 @@
-print("For exit type \"exit\"")
-while True:
-    data = input("write binary text: ")
-    if data == "exit":
-        break
-    data = data.split(" ")
-    for i in data:
-        print(chr(int(i, 2)),end="")
-    print()
